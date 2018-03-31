c2fb6ba35d30ad11e64a16dd1bcface79ad1c4ad
Rehana Mahfuz <rmahfuz@purdue.edu>
2018-03-31 19:28:21 -0400

    Delete tst.c

--- lab4/one-level/tst.c
+++
@@ -1,9 +0,0 @@
-
-#define MEM_MAX_VIRTUAL_ADDRESS (1<<22) -1
-#include<stdio.h>
-int main()
-{
-	printf("Ours: %d\n", 0x3FFFFF & (~0x3));
-	printf("adjunker: %d\n", MEM_MAX_VIRTUAL_ADDRESS - 3);
-	return 0;
-}
